fb001c4280069a37b786783f4ccc265112e80afb
feli <felfeng@ucdavis.edu>
2025-01-23 11:58:47 -0800

    change to top 150 movies

--- app/api/tmdb/route.ts
+++ app/api/tmdb/route.ts
@@ -1,38 +1,38 @@
-import { NextResponse } from 'next/server';
-import axios from 'axios';
+import { NextResponse } from "next/server";
+import axios, { all } from "axios";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const endpoint = searchParams.get('endpoint');
-  const movieId = searchParams.get('movieId');
+  const endpoint = searchParams.get("endpoint");
+  const movieId = searchParams.get("movieId");
 
   if (!endpoint) {
     return NextResponse.json(
-      { error: 'Endpoint parameter is required' },
+      { error: "Endpoint parameter is required" },
       { status: 400 }
     );
   }
 
   try {
-    if (endpoint === 'popular') {
-      const pages = await Promise.all([1, 2, 3].map(page =>
-        axios.get(`${process.env.TMDB_API_URL}/discover/movie`, {
-          params: {
-            api_key: process.env.TMDB_API_KEY,
-            sort_by: 'vote_count.desc',
-            'vote_count.gte': 10000,
-            'vote_average.gte': 7.0,
-            page: page,
-          },
-        })
-      ));
-      const allResults = pages.flatMap(response => response.data.results);
-      const top50 = allResults.slice(0, 50);
-
-      return NextResponse.json({ results: top50 });
+    if (endpoint === "popular") {
+      const pages = await Promise.all(
+        [1, 2, 3, 4, 5, 6, 7, 8].map((page) =>
+          axios.get(`${process.env.TMDB_API_URL}/discover/movie`, {
+            params: {
+              api_key: process.env.TMDB_API_KEY,
+              sort_by: "vote_count.desc",
+              "vote_count.gte": 10000,
+              "vote_average.gte": 8.0,
+              page: page,
+            },
+          })
+        )
+      );
+      const allResults = pages.flatMap((response) => response.data.results);
+      return NextResponse.json({ results: allResults });
     }
-    
-    if (endpoint === 'credits' && movieId) {
+
+    if (endpoint === "credits" && movieId) {
       const response = await axios.get(
         `${process.env.TMDB_API_URL}/movie/${movieId}/credits`,
         {
@@ -44,14 +44,11 @@ export async function GET(request: Request) {
       return NextResponse.json(response.data);
     }
 
-    return NextResponse.json(
-      { error: 'Invalid endpoint' },
-      { status: 400 }
-    );
+    return NextResponse.json({ error: "Invalid endpoint" }, { status: 400 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { error: 'Failed to fetch data from TMDB' },
+      { error: "Failed to fetch data from TMDB" },
       { status: 500 }
     );
   }
